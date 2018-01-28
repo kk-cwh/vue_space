3e3ca312b74b07c47760451902147840511e974d
yake <jaak@yakedeMacBook-Pro.local>
2018-01-28 22:51:30 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+   {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  width: 100px;
+  height: 100px;
+  background: url('/static/uicon.jpg') ;
+  background-size: 100% 100%;
+  border-radius: 50%;
+  box-shadow: 0 0  5px 5px rgb(27, 185, 27) ;
+}
+</style>
---
+++ src/components/Select.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+   
+     <ul class="store">
+         <li v-for="(item,index) in ulDatas" :key="item+index" v-text="item"> </li> 
+      </ul>  
+<input  class="search" v-model="search" />
+   <ul class="searchData"> 
+      <li v-for="item in searchData" :key="item" v-text="item" @click="addToUl(item)"> </li> 
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Select',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      search: '',
+      items: ['中国', '美国', '韩国', '日本'],
+      ulDatas: []
+    }
+  },
+  computed: {
+    searchData: function () {
+      if (this.search) {
+        return this.items.filter((item) => { return item.indexOf(this.search) > -1 })
+      }
+      return []
+    }
+  },
+  methods: {
+    addToUl (item) {
+      this.ulDatas.push(item)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.search{
+   border: 1px solid #000000; 
+  box-sizing: border-box; 
+  /* outline:none; */
+}
+.searchData{
+  list-style: none;
+  background: #fff;
+}
+.searchData  li{
+  background: #fff;
+ 
+}
+.searchData li:hover{
+background: #efefef;
+}
+.store{
+ list-style: none;
+}
+.store li{
+ display: inline-block;
+ margin: 2px;
+}
+</style>
