9ef570f1ac86e13ce49183cea104e7005d2b8228
yake <jaak@yakedeMacBook-Pro.local>
2018-01-31 23:37:42 +0800

    add select search

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-   
-     <ul class="store">
-         <li v-for="(item,index) in ulDatas" :key="item+index" v-text="item"> </li> 
-      </ul>  
-<input  class="search" v-model="search" />
-   <ul class="searchData"> 
-      <li v-for="item in searchData" :key="item" v-text="item" @click="addToUl(item)"> </li> 
-   </ul>
+
+    <ul class="store">
+      <li v-for="(item,index) in ulDatas" :key="item+index" > 
+      <span v-text="item"></span>
+      <i class="fa fa-times " @click="del(index)"></i>
+      </li>
+      <li><input class="search" v-model="search" /></li>
+    </ul>
+    
+    <ul class="searchData">
+      <li v-for="item in searchData" :key="item" v-text="item" @click="addToUl(item)"> </li>
+    </ul>
   </div>
 </template>
 
@@ -25,7 +29,9 @@ export default {
   computed: {
     searchData: function () {
       if (this.search) {
-        return this.items.filter((item) => { return item.indexOf(this.search) > -1 })
+        return this.items.filter(item => {
+          return item.indexOf(this.search) > -1
+        })
       }
       return []
     }
@@ -33,6 +39,9 @@ export default {
   methods: {
     addToUl (item) {
       this.ulDatas.push(item)
+    },
+    del (index) {
+      this.ulDatas.splice(index, 1)
     }
   }
 }
@@ -40,27 +49,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.search{
-   border: 1px solid #000000; 
-  box-sizing: border-box; 
+.search {
+  border: 1px solid #000000;
+  box-sizing: border-box;
   /* outline:none; */
 }
-.searchData{
+.searchData {
   list-style: none;
   background: #fff;
 }
-.searchData  li{
+.searchData li {
   background: #fff;
- 
 }
-.searchData li:hover{
-background: #efefef;
+.searchData li:hover {
+  background: #efefef;
 }
-.store{
- list-style: none;
+.store {
+  list-style: none;
+  padding: 5px;
+  border: 1px solid #bbb9b9eb;
+  box-sizing: border-box;
 }
-.store li{
- display: inline-block;
- margin: 2px;
+.store li {
+  display: inline-block;
+  background: #fff;
+  border:1px solid #b3b3b3;
+  box-sizing: border-box;
+  border-radius: 3px;
+  padding: 5px;
+  margin-left: 5px;
+}
+.store li:last-child{
+ border:none;
+ padding: 0;
+ margin: 0;
+}
+.store li:last-child  input{
+  border:none;
+   outline: none;
+}
+.store li i{
+cursor: pointer;
 }
 </style>
